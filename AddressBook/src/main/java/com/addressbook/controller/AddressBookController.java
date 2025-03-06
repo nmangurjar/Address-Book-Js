64f7c48f3d14c04bd5e6b1d33fc6ff2b0863775e
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-03-06 21:53:28 +0530

    [Naman] Add. Determined the Logging Levels,

--- AddressBook/src/main/java/com/addressbook/controller/AddressBookController.java
+++ AddressBook/src/main/java/com/addressbook/controller/AddressBookController.java
@@ -21,24 +21,28 @@ public class AddressBookController {
 
     @GetMapping
     public ResponseEntity<List<AddressBook>> getContacts() {
+        log.info("Fetching all contacts");
         List<AddressBook> addressList = addressBookService.getAllContacts();
         return new ResponseEntity<>(addressList, HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<String> addContact(@RequestBody AddressBookDTO addressBookDTO) {
+        log.info("Adding new contact: {}", addressBookDTO.getName());
         String response = addressBookService.addContact(addressBookDTO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> updateContact(@PathVariable Long id, @RequestBody AddressBookDTO addressBookDTO) {
+        log.info("Updating contact with ID: {}", id);
         String response = addressBookService.updateContact(id, addressBookDTO);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteContact(@PathVariable Long id) {
+        log.info("Deleting contact with ID: {}", id);
         String response = addressBookService.deleteContact(id);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- AddressBook/src/main/java/com/addressbook/service/AddressBookService.java
+++ AddressBook/src/main/java/com/addressbook/service/AddressBookService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 @Slf4j
 public class AddressBookService {
@@ -15,12 +16,14 @@ public class AddressBookService {
     private Long idCounter = 1L;
 
     public List<AddressBook> getAllContacts() {
+        log.info("Retrieving all contacts");
         return contactList;
     }
 
     public String addContact(AddressBookDTO addressBookDTO) {
         AddressBook newContact = new AddressBook(idCounter++, addressBookDTO.getName(), addressBookDTO.getEmail(), addressBookDTO.getPhone());
         contactList.add(newContact);
+        log.info("Contact Added: {}", newContact.getName());
         return "Contact Added: " + newContact.getName();
     }
 
@@ -30,9 +33,11 @@ public class AddressBookService {
                 contactList.remove(contact);
                 AddressBook updatedContact = new AddressBook(id, addressBookDTO.getName(), addressBookDTO.getEmail(), addressBookDTO.getPhone());
                 contactList.add(updatedContact);
+                log.info("Updated Contact: {} with ID: {}", updatedContact.getName(), id);
                 return "Updated Contact: " + updatedContact.getName() + " with ID: " + id;
             }
         }
+        log.warn("Contact Not Found with ID: {}", id);
         return "Contact Not Found with ID: " + id;
     }
 
@@ -40,9 +45,11 @@ public class AddressBookService {
         for (AddressBook contact : contactList) {
             if (contact.getId().equals(id)) {
                 contactList.remove(contact);
+                log.info("Deleted Contact with ID: {}", id);
                 return "Deleted Contact with ID: " + id;
             }
         }
+        log.warn("Contact Not Found with ID: {}", id);
         return "Contact Not Found with ID: " + id;
     }
 }
